4d17c58ac394057e12d03efe50e480244a1272d5
ThanNong <nguyenhoangtai2k@gmail.com>
2022-08-12 13:50:35 +0700

    Add many class

---
+++ BusinessLogic/UseCase/Crud/CartDetail/Command/DeleteCartDetail/DeleteCartDetailRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.UseCase.Crud.CartDetail.Command.DeleteCartDetail
+{
+    internal class DeleteCartDetailRequest
+    {
+    }
+}
---
+++ BusinessLogic/UseCase/Crud/DetailBuyBill/Command/DeleteDetailBuyBill/DeleteDetailBuyBillHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.UseCase.Crud.DetailBuyBill.Command.DeleteDetailBuyBill
+{
+    internal class DeleteDetailBuyBillHandler
+    {
+    }
+}
